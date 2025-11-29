cf201f4c1867ab2ee0179fec3c4d368511badc47
Yogaone <yogawanadityapratama@gmail.com>
2025-11-29 18:10:21 +0700

    refactor: update sidebar text size and change app title to "MekarJS ERP Core Service"
    - Adjusted text size in Sidebar component from text-xl to text-xs for better readability.
    - Updated the title in the _app.tsx file to "MekarJS ERP Core Service".
    - Changed package name in package.json and package-lock.json from "corequarry-core-service" to "mekarjs-erp-core-service".
    - Updated AWS SDK dependencies to version 3.940.0 in package-lock.json.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -47,7 +47,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen = true, onClose }) => {
         {menu.path && !menu.subMenu ? (
           <Link
             href={menu.path}
-            className={`flex items-center justify-between w-full px-3 py-2 text-xl text-left rounded-lg transition-all duration-200 group ${
+            className={`flex items-center justify-between w-full px-3 py-2 text-xs text-left rounded-lg transition-all duration-200 group ${
               isActive
                 ? "bg-primary text-white"
                 : "hover:bg-gray-200 text-gray-700"
@@ -59,7 +59,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen = true, onClose }) => {
                   isActive ? "text-white" : "text-primary"
                 }`}
               />
-              <span className="font-medium">{menu.title}</span>
+              <span className="text-xs font-medium">{menu.title}</span>
             </div>
           </Link>
         ) : (
@@ -77,7 +77,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen = true, onClose }) => {
                   isActive ? "text-white" : "text-primary"
                 }`}
               />
-              <span className="font-medium">{menu.title}</span>
+              <span className="text-xs font-medium">{menu.title}</span>
             </div>
             {menu.subMenu && (
               <div
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <title>CoreQuarry</title>
+        <title>MekarJS ERP Core Service</title>
         <link rel="icon" href="/favicon/icon.svg" type="image/svg+xml" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
